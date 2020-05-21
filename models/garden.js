bd6516c401d99befe79ebdfac4f3c2f541ecd154
Diloretodg <diloretodg74122@gmail.com>
2020-05-21 16:17:01 -0400

    models init

---
+++ models/garden.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// todo: outline data entries and potential
+const gardenSchema = new Schema({
+
+})
+
+const Garden = mongoose.model("Garden", gardenSchema);
+
+module.exports = Garden;
